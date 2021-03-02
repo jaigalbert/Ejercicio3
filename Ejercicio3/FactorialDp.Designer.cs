8148722890933d0d38470b810c407547818a36bb
Alberto Iribe Gonzalez <jaigalbert@gmail.com>
2021-03-01 19:33:24 -0700

    Agregar archivos de proyecto.

---
+++ Ejercicio3/FactorialDp.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Ejercicio3
+{
+    partial class FactorialDP
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FactorialDP));
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnCalcular.Location = new System.Drawing.Point(236, 20);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 33);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblNumero.Location = new System.Drawing.Point(23, 27);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(58, 17);
+            this.lblNumero.TabIndex = 0;
+            this.lblNumero.Text = "Numero";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtNumero.Location = new System.Drawing.Point(106, 25);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 23);
+            this.txtNumero.TabIndex = 1;
+            this.txtNumero.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtResultado.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtResultado.Location = new System.Drawing.Point(106, 69);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(100, 23);
+            this.txtResultado.TabIndex = 4;
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblResultado.Location = new System.Drawing.Point(23, 69);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(72, 17);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // FactorialDP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 127);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.btnCalcular);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FactorialDP";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Factorial DP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Ejercicio3/FactorialDp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio3
+{
+    public partial class FactorialDP : Form
+    {
+        public FactorialDP()
+        {
+            InitializeComponent();
+        }
+        //Calculo de factorial
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (esEntero(txtNumero.Text))
+            {
+                int Numero = Int32.Parse(txtNumero.Text), Resultado=1;
+                for (int i = Numero ; i > 0; --i) {
+                    Resultado = (Resultado * i);
+                }
+                txtResultado.Text = Resultado.ToString();
+                MessageBox.Show("El factorial de "+txtNumero.Text+" es igual a "+txtResultado.Text);
+            }
+            else {
+                MessageBox.Show("El valor capturado no es entero");
+            }
+        }
+        //Validacion de entero para obtener factorial
+        public bool esEntero(string numero)
+        {
+            try
+            {
+                Int32.Parse(numero);
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+    
+}
